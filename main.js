f28b54668e28bfd7d1445a9e42bddc57632f9a82
Yasmine135 <132566027+Yasmine135@users.noreply.github.com>
2024-10-08 10:40:14 -0300

    hv

---
+++ main.js
@@ -0,0 +1,9 @@
+function mostraResposta (cartao__conteudo){
+   const resposta = cartao__conteudo.querySelector('.cartao__conteudo__resposta');
+   if (resposta.style.display == 'none'){
+       resposta.style.display == 'block';
+   }
+   else (){
+       resposta.style.display == 'none'
+   }
+}
