2d9dd62e585f8d9c2bc9ef2c044e346b1c393819
Yasmine135 <132566027+Yasmine135@users.noreply.github.com>
2024-10-24 11:17:54 -0300

    hlob

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function mostraResposta (cartao__conteudo){
    const resposta = cartao__conteudo.querySelector('.cartao__conteudo__resposta');
-   if (resposta.style.display == 'none'){
-       resposta.style.display == 'block';
+   if (resposta.style.display === 'none'){
+       resposta.style.display = 'block';
    }
 
    else {
-       resposta.style.display == 'none'
+       resposta.style.display = 'none'
    }
 }
